1842e3f967bb50182c2427f843093ee206ca1e1b
JakDoh <jak.doh@gmail.com>
2023-07-21 23:39:27 +0200

    bugfix

--- tendabeli/switch.py
+++ tendabeli/switch.py
@@ -19,7 +19,7 @@ async def async_setup_platform(
     discovery_info: DiscoveryInfoType | None = None) -> None:
 
     async def remove_entity(entity_id):
-        entity_registry = await async_get(hass)
+        entity_registry = async_get(hass)
         entity_entry = entity_registry.async_get(entity_id)
         if entity_entry:
             await entity_registry.async_remove(entity_entry.entity_id)
